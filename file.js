0ccf81a00bcc7046edab0aa5eecf2e660e0af2fa
Edd <fausten17otmail.com>
2021-05-21 10:44:24 -0500

    some functions for loop if statment

--- file.js
+++ file.js
@@ -23,6 +23,7 @@ console.log(list[1]+list[0], list[list.length-1]); // no existe list[-1]
 var list = [1,2,-3172, 3, 45, 3, 3];
 var fruits = ["Banana", "Orange", "Apple", "Mango"];
 
+
 console.log("Apple" in fruits);
 
 var arr = Array.from("Hello"); // Array.from(obj) == list(obj)
@@ -48,3 +49,34 @@ var str = "Hello, world!";
 console.log(str.length); // obj.len == len(obj),  pero hay que ponerlo en una variable
 
 console.log("Hello".length);
+
+
+const str = "a=1, b=2, c=3, d=4";
+const str2 = str.replace(/ /g,'');
+console.log(str2)
+var dict = {};
+var str3 = str2.split(',');
+console.log(typeof str3, str3);
+for (i=0; i<str3.length; i++) {
+    console.log(str3[i], str3[i][0], str3[i][1], str3[i][2]);
+};
+console.log(dict);
+
+
+
+function strToHash(str){
+    console.log(str)
+    const str2 = str.replace(/ /g,'');
+    var dict = {};
+    var list = str2.split(',');
+    for (i=0; i<list.length; i++) {
+      var x = list[i].split('=');
+      dict[x[0]] = parseInt(x[1])
+    }
+    return dict
+  }
+
+var str = "";
+if (str) {
+    console.log("Hey")
+}

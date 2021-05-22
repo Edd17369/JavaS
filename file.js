af3d1afba8eff195e25661b259426ae3b9dd9b84
Edd <fausten17otmail.com>
2021-05-22 15:53:08 -0500

    5/22

--- file.js
+++ file.js
@@ -1,37 +1,33 @@
 // in-line comment
 /* multi-line comment */
-var a;
-console.log(a);
-a = 7;
-console.log(a)
 
-var a = 1;
-a = a+5;
-a+=10;
-console.log(a);
+
+// Built in functions
+Math.max(val1,val2);
+
+
+// Logical Operators
+// || or
+// && and
+// ! not
 
 
 // Arrays
 
-// Attrs
-var list = [1,2,3];
+var list = [1,2,3]; // Usa este para crear arrays
+var list2 = new Array(1,2,3);
 var len = list.length; // Su unico attr xd
 console.dir(list); // solo funciona en el web browser
 console.log(list[1]+list[0], list[list.length-1]); // no existe list[-1]
 
-// Methods
 var list = [1,2,-3172, 3, 45, 3, 3];
 var fruits = ["Banana", "Orange", "Apple", "Mango"];
 
-
-console.log("Apple" in fruits);
-
+var s = fruits.slice(0,2) // como fruits[0:2], no incluye al ultimo
+list.includes(val) // si val esta en array, no existe 'in' en JS  
 var arr = Array.from("Hello"); // Array.from(obj) == list(obj)
-
-function fcn(n) {return n == 3 };
-var age = list.find(fcn); // find the first elem that pass the test
-
-var concat = list.concat(fruits); // concat() == extend()
+list.find(fcn); // find the first elem that pass the test
+list.concat(fruits); // concat() == extend()
 
 
 
@@ -80,3 +76,30 @@ var str = "";
 if (str) {
     console.log("Hey")
 }
+
+// Set
+var s = new Set([1,2,3]); // new set, si quieres que sea vacio: new Set(); no intentes s = ();
+s.add(val) 
+s.remove(val) 
+s.clear() // limpia el set
+s.has(val) // True si val in s
+
+
+// Dictionary
+
+var dict = {};
+dict[new_key] = new_value;
+
+var array = [3,2,1,2,3,3,2,2,2,3,4];
+var s = {};
+var ans = 0;
+var i;
+for (i=0; i<array.length; i++) {
+  if (!(array[i] in s)) {
+    s[array[i]] = 1;
+  }
+  else {
+    s[array[i]]++;
+  }
+}
+  console.log(s)
--- file.py
+++ file.py
@@ -2,3 +2,13 @@ def my_sum(a,b):
     return a+b
 
 print(my_sum(3,-44)) # Hay que correr la funcion cada ocacion
+
+
+def fan(str):
+    A = str2.split(', ')
+    H = {}
+    for x in A:
+        H[x[0]] = int(x[-1])
+    return H
+
+print(fan("a=1, b=2, c=3, d=4"))
